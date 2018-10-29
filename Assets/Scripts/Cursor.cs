78de54cae760e8214a9c157eeda3e528f5e3b286
Nannings <mrnannings@gmail.com>
2018-10-29 11:29:11 +0100

    10;

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    CameraRaycaster cameraRaycaster;
+
+    private void Start()
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+
+    private void Update()
+    {
+        print(cameraRaycaster.layerHit);
+    }
+}
