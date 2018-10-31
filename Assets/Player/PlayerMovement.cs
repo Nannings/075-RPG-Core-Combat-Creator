a49ab69a52b1bb0cb75cb4a46647ed3b813171f8
Nannings <mrnannings@gmail.com>
2018-10-31 10:05:57 +0100

    17;

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,13 +5,14 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
-
     [SerializeField] float walkMoveStopRadius = 0.2f;
 
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
+
+    bool isInDirectMode = false;
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -21,6 +22,35 @@ public class PlayerMovement : MonoBehaviour
 
     // Fixed update is called in sync with physics
     private void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            isInDirectMode = !isInDirectMode;
+        }
+
+        if (isInDirectMode)
+        {
+            ProcessDirectMovement();
+        }
+        else
+        {
+            ProcessMouseMovement();
+        }
+    }
+
+    private void ProcessDirectMovement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        m_Character.Move(m_Move, false, false);
+    }
+
+    private void ProcessMouseMovement()
     {
         if (Input.GetMouseButton(0))
         {
