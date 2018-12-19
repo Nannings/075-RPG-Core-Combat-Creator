898e230d304132e1d4a37d27e367e3eda620354b
Nannings <mrnannings@gmail.com>
2018-12-19 17:51:38 +0100

    3 91;

---
+++ Assets/_Weapons/Projectile.cs
@@ -0,0 +1,52 @@
+﻿using RPG.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.weapons
+{
+    public class Projectile : MonoBehaviour
+    {
+
+        [SerializeField] float projectileSpeed; // Note other classes can set
+
+        const float DESTROY_DELAY = 0.01f;
+        [SerializeField] GameObject shooter;
+        float damageCaused;
+
+        public void SetShooter(GameObject shooter)
+        {
+            this.shooter = shooter;
+        }
+
+        public void SetDamage(float damage)
+        {
+            damageCaused = damage;
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            var layerCollideWith = collision.gameObject.layer;
+            if (layerCollideWith != shooter.layer)
+            {
+                DamageIfDamageables(collision);
+            }
+        }
+
+        private void DamageIfDamageables(Collision collision)
+        {
+            Component damagableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
+            if (damagableComponent)
+            {
+                (damagableComponent as IDamageable).TakeDamage(damageCaused);
+            }
+            Destroy(gameObject, DESTROY_DELAY);
+        }
+
+        internal float GetDefaultLaunchSpeed()
+        {
+            return projectileSpeed;
+        }
+    }
+}
