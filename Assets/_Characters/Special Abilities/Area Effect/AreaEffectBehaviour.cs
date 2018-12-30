a926699a07aacb85e4561c8804ec789850becfaa
Nannings <mrnannings@gmail.com>
2018-12-30 15:13:59 +0100

    3 116;

---
+++ Assets/_Characters/Special Abilities/Area Effect/AreaEffectBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core;
+
+namespace RPG.Characters
+{
+    public class AreaEffectBehaviour : MonoBehaviour, ISpecialAbility
+    {
+        AreaEffectConfig config;
+
+        public void SetConfig(AreaEffectConfig configToSet)
+        {
+            this.config = configToSet;
+        }
+
+        private void Start()
+        {
+            print("Area effect to " + gameObject.name);
+        }
+
+        public void Use(AbilityUseParams useParams)
+        {
+            print("Area effect used by " + gameObject.name);
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position, config.GetRadius(), Vector3.up, config.GetRadius());
+            foreach (RaycastHit hit in hits)
+            {
+                var damageable = hit.collider.gameObject.GetComponent<IDamageable>();
+                if (damageable != null)
+                {
+                    float damageToDeal = useParams.baseDamage + config.GetDamageToEachTarget();
+                    damageable.TakeDamage(damageToDeal);
+                }
+            }
+        }
+    }
+}
