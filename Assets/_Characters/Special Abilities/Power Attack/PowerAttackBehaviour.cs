5912a3170271f5c9705ff17bba1e67e77d160957
Nannings <mrnannings@gmail.com>
2019-01-12 20:45:33 +0100

    4 143;

--- Assets/_Characters/Special Abilities/Power Attack/PowerAttackBehaviour.cs
+++ Assets/_Characters/Special Abilities/Power Attack/PowerAttackBehaviour.cs
@@ -6,13 +6,7 @@ namespace RPG.Characters
 {
     public class PowerAttackBehaviour : AbilityBehaviour
     {
-        PowerAttackConfig config;
-		AudioSource audioSource = null;
-
-        public void SetConfig(PowerAttackConfig configToSet)
-        {
-            this.config = configToSet;
-        }
+        AudioSource audioSource = null;
 
         // Use this for initialization
         void Start()
@@ -36,19 +30,9 @@ namespace RPG.Characters
 			audioSource.Play();
         }
 
-		private void PlayParticleEffect()
-		{
-            var particlePrefab = config.GetParticlePrefab();
-            var prefab = Instantiate(particlePrefab, transform.position, particlePrefab.transform.rotation);
-            // TODO decide if particle system attaches to player
-            ParticleSystem myParticleSystem = prefab.GetComponent<ParticleSystem>();
-			myParticleSystem.Play();
-			Destroy(prefab, myParticleSystem.main.duration);
-		}
-
         private void DealDamage(AbilityUseParams useParams)
         {
-            float damageToDeal = useParams.baseDamage + config.GetExtraDamage();
+            float damageToDeal = useParams.baseDamage + (config as PowerAttackConfig).GetExtraDamage();
             useParams.target.TakeDamage(damageToDeal);
         }
     }
