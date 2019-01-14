4808d6d5609bb27809255eba91b5b3abbc7df994
Nannings <mrnannings@gmail.com>
2019-01-14 19:50:38 +0100

    4 146;

--- Assets/_Characters/Player/Player.cs
+++ Assets/_Characters/Player/Player.cs
@@ -50,6 +50,11 @@ namespace RPG.Characters
             AttachInitialAbilities();
         }
 
+        public void PutWeaponInHand(Weapon weaponConfig)
+        {
+            print(1212);
+        }
+
         private void AttachInitialAbilities()
         {
             for (int abilityIndex = 0; abilityIndex < abilities.Length; abilityIndex++)
---
+++ Assets/_Weapons/WeaponPickupPoint.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Characters;
+
+namespace RPG.Weapons
+{
+    [ExecuteInEditMode]
+    public class WeaponPickupPoint : MonoBehaviour
+    {
+        [SerializeField] Weapon weaponConfig;
+        [SerializeField] AudioClip pickUpSFX;
+
+        AudioSource audioSource;
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        private void Update()
+        {
+            if (!Application.isBatchMode)
+            { 
+                DestroyChildren();
+                InstantiateWeapon();
+            }
+        }
+
+        private void DestroyChildren()
+        {
+            foreach (Transform child in transform)
+            {
+                DestroyImmediate(child.gameObject);
+            }
+        }
+
+        private void InstantiateWeapon()
+        {
+            var weapon = weaponConfig.GetWeaponPrefab();
+            weapon.transform.position = Vector3.zero;
+            Instantiate(weapon, gameObject.transform);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            FindObjectOfType<Player>().PutWeaponInHand(weaponConfig);
+            audioSource.clip = pickUpSFX;
+            audioSource.Play();
+        }
+    }
+}
