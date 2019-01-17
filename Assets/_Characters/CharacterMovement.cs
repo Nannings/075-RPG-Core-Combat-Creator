87988a1656f68e684e80dc6729b718daceed8c62
Nannings <mrnannings@gmail.com>
2019-01-17 22:41:57 +0100

    4 151;

--- Assets/_Characters/CharacterMovement.cs
+++ Assets/_Characters/CharacterMovement.cs
@@ -10,17 +10,23 @@ namespace RPG.Characters
     public class CharacterMovement : MonoBehaviour
     {
         [SerializeField] float stoppingDistance = 1f;
+        [SerializeField] float moveSpeedMultiplier = .7f;
 
         ThirdPersonCharacter character;
         Vector3 clickPoint;
         GameObject walkTarget;
         NavMeshAgent agent;
+        Animator animator;
+        Rigidbody rigidbody;
 
         void Start()
         {
             CameraRaycaster cameraRaycaster = Camera.main.GetComponent<CameraUI.CameraRaycaster>();
             character = GetComponent<ThirdPersonCharacter>();
             walkTarget = new GameObject("walkTarget");
+
+            animator = GetComponent<Animator>();
+            rigidbody = GetComponent<Rigidbody>();
 
             agent = GetComponent<NavMeshAgent>();
             agent.updateRotation = false;
@@ -56,6 +62,16 @@ namespace RPG.Characters
             if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(1))
             {
                 agent.SetDestination(enemy.transform.position);
+            }
+        }
+
+        private void OnAnimatorMove()
+        {
+            if (Time.deltaTime > 0)
+            {
+                Vector3 velocity = (animator.deltaPosition * moveSpeedMultiplier) / Time.deltaTime;
+                velocity.y = rigidbody.velocity.y;
+                rigidbody.velocity = velocity;
             }
         }
     }
