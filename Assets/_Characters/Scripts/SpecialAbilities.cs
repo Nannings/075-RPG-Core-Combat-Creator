76835f6148d9c4cde0523fd8bb79755d17a5b07e
Nannings <mrnannings@gmail.com>
2019-01-18 16:59:43 +0100

    4 155;

---
+++ Assets/_Characters/Scripts/SpecialAbilities.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Characters
+{
+    public class SpecialAbilities : MonoBehaviour
+    {
+        [SerializeField] AbilityConfig[] abilities;
+        [SerializeField] Image energyBar;
+        [SerializeField] float maxEnergyPoints = 100f;
+        [SerializeField] float regenPointsPerSecond = 1f;
+
+        float currentEnergyPoints;
+        AudioSource audioSource;
+
+        float energyAsPercent
+        {
+            get { return currentEnergyPoints / maxEnergyPoints; }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+            currentEnergyPoints = maxEnergyPoints;
+            AttachInitialAbilities();
+            UpdateEnergyBar();
+        }
+
+        void Update()
+        {
+            if (currentEnergyPoints < maxEnergyPoints)
+            {
+                AddEnergyPoints();
+                UpdateEnergyBar();
+            }
+        }
+
+        private void AttachInitialAbilities()
+        {
+            for (int abilityIndex = 0; abilityIndex < abilities.Length; abilityIndex++)
+            {
+                abilities[abilityIndex].AttachAbilityTo(gameObject);
+            }
+        }
+
+        public void AttemptSpecialAbility(int abilityIndex)
+        {
+            var energyCost = abilities[abilityIndex].GetEnergyCost();
+
+            if (energyCost <= currentEnergyPoints)
+            {
+                ConsumeEnergy(energyCost);
+                print("special " + abilityIndex);
+            }
+            else
+            {
+            }
+        }
+
+        public int GetNumberOfAbilities()
+        {
+            return abilities.Length;
+        }
+
+        private void AddEnergyPoints()
+        {
+            var pointsToAdd = regenPointsPerSecond * Time.deltaTime;
+            currentEnergyPoints = Mathf.Clamp(currentEnergyPoints + pointsToAdd, 0, maxEnergyPoints);
+        }
+
+        public void ConsumeEnergy(float amount)
+        {
+            float newEnergyPoints = currentEnergyPoints - amount;
+            currentEnergyPoints = Mathf.Clamp(newEnergyPoints, 0, maxEnergyPoints);
+            UpdateEnergyBar();
+        }
+
+        private void UpdateEnergyBar()
+        {
+            energyBar.fillAmount = energyAsPercent;
+        }
+    }
+}
