271f534beffc3e18ef2e0c29894fa4d4cb021938
Nannings <mrnannings@gmail.com>
2019-01-20 20:57:55 +0100

    4 165;

--- Assets/_Characters/Enemies/EnemyAI.cs
+++ Assets/_Characters/Enemies/EnemyAI.cs
@@ -4,25 +4,61 @@ using UnityEngine;
 
 namespace RPG.Characters
 {
+    [RequireComponent(typeof(Character))]
     [RequireComponent(typeof(WeaponSystem))]
     public class EnemyAI : MonoBehaviour
     {
         [SerializeField] float chaseRadius = 6f;
 
-        bool isAttacking = false;
         PlayerMovement player = null;
+        Character character;
         float currrentWeaponRange;
+        float distanceToPlayer;
+
+        enum State { idle, patrolling, attacking, chasing};
+        State state = State.idle;
 
         void Start()
         {
+            character = GetComponent<Character>();
             player = FindObjectOfType<PlayerMovement>();
         }
 
         void Update()
         {
-            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                Time.timeScale = 0;
+            }
+
+            distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             WeaponSystem weaponSystem = GetComponent<WeaponSystem>();
             currrentWeaponRange = weaponSystem.GetCurrentWeapon().GetMaxAttackRange();
+            if(distanceToPlayer > chaseRadius && state != State.patrolling)
+            {
+                StopAllCoroutines();
+                state = State.patrolling;
+            }
+            if (distanceToPlayer <= chaseRadius && state != State.chasing)
+            {
+                StopAllCoroutines();
+                StartCoroutine(ChasePlayer());
+            }
+            if(distanceToPlayer <= currrentWeaponRange && state != State.attacking)
+            {
+                StopAllCoroutines();
+                state = State.attacking;
+            }
+        }
+
+        IEnumerator ChasePlayer()
+        {
+            state = State.chasing;
+            while (distanceToPlayer >= currrentWeaponRange)
+            {
+                character.SetDestination(player.transform.position);
+                yield return new WaitForEndOfFrame();
+            }
         }
 
         void OnDrawGizmos()
